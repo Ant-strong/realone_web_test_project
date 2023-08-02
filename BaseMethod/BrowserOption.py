1f4f411f0f033535f8bd416358d9391b905b378d
real_company <186612857@qq.com>
2023-08-02 18:01:47 +0800

    新编辑BrowserOption.py和新增加drivers

--- BaseMethod/BrowserOption.py
+++ BaseMethod/BrowserOption.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# @Author  : zm
+# -*- coding: uft-8 -*-
+# @Author : ZM
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+
+# 浏览器操作
+class BrowserDriver(object):
+    drivers_base_path = os.getcwd() + os.sep + 'drivers'
+    chrome_driver_path = drivers_base_path + 'chromedriver.exe'
+
+    def open_browser(self, url):
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--start-maximized') # 窗口最大化启动
+        browser = webdriver.Chrome()
